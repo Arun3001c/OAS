2b95677b96ad30f97dae23b09a5506c60beefc60
Arun <pmsak28@gmail.com>
2025-08-04 19:10:37 +0530

    added contact .jsx

--- frontend/src/components/pages/Home/Home.jsx
+++ frontend/src/components/pages/Home/Home.jsx
@@ -2,7 +2,7 @@
 const Home = () => {
   return (
     <div style={{ padding: '2rem' }}>
-      <h1>Welcome to your Dashboard 🎉</h1>
+      <h1>Welcome to your Home 🎉</h1>
     </div>
   );
 };
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true, // Allow access from local network
-    port: 5173, // Optional, default is 5173
-  },
-})
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      port: 5173,
+    },
+    define: {
+      'process.env': env
+    }
+  };
+});
